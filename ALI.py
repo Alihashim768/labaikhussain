adcfb2f41270cc0d521fc22bcfbd7a2e1c82bf7c
Alihashim768 <85179965+Alihashim768@users.noreply.github.com>
2022-05-31 03:34:30 +0500

    Update ALI.py

--- ALI.py
+++ ALI.py
@@ -1,15 +1,34 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-os.system('git pull')
-import requests
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from ALI import alibuy
-    alibuy()
-elif bit == '32bit':
-    from ALI32 import alibuy
-    alibuy()
+def Subscraption():
+	key1=open('/data/data/com.termux/files/home/labaikhussain/ALI.PY', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://pastebin.com/raw/gL0egEPn").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Main()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+		time.sleep(1)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32m Ali Toll Paid You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m Note : Paid Tolls  HA JANI LOG \033[1;37m")
+		print ("")
+		print(" Your Key is Not Approved ")
+		print("")
+		print(" Copy And Send Key To Admin")
+		print ("")
+		print (" Your Key : "+ak+ahsan+key1)
+		print ("")
+		name = input(" Your Name : ")
+		print ("")
+		input(" Press Enter To Send Key")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
+		os.system('am start https://wa.me/+9203048018571?text=' + tks)
+		Subscraption()        
+Subscraption()
